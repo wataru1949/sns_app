27e6f858d2f60b1f99797c42b42c4d382849976d
wataru1949 <e.phedra58@icloud.com>
2020-05-11 23:06:45 +0900

    edit controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
-  
+  before_action :set_post, only: [:edit, :update, :show]
+
   def index
+    @posts = Post.order(created_at: :desc)
   end
 
   def new
@@ -16,8 +18,29 @@ class PostsController < ApplicationController
     end
   end
 
+  def show
+  end
 
+  def edit
+    # if @post.user_id != current_user.id
+    #   redirect_to root_path
+    # end
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
   def post_params
     params.require(:post).permit(:title, :event, :detail, :prefecture_id, :accept, :date, :start_time, :end_time)#.merge(user_id: current_user.id )
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
