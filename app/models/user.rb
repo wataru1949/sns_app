f45a866b19bb40086725cfc6e734824dde2e5da2
wataru1949 <e.phedra58@icloud.com>
2020-05-14 23:49:30 +0900

    create users model

---
+++ app/models/user.rb
@@ -0,0 +1,41 @@
+class User < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  enum gender: { male: 1, female: 2, others: 3 }
+  enum age: { teens: 1, twenties: 2, thirties: 3, forties: 4, fifties: 5, over_sixties: 6 }
+
+  belongs_to_active_hash :prefecture
+  has_many :posts
+  has_many :comments
+  # has_many :pictures, as: :imageable
+  # has_and_belongs_to_many :clubs
+  # validates :nickname, :profile, :gender, :age, presence: true
+end
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  age                    :integer          not null
+#  email                  :string(255)      default(""), not null
+#  encrypted_password     :string(255)      default(""), not null
+#  gender                 :integer          not null
+#  nickname               :string(255)      not null
+#  profile                :string(255)      not null
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string(255)
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  prefecture_id          :integer
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_30_025620) do
+ActiveRecord::Schema.define(version: 2020_05_14_073417) do
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "comment"
+    t.bigint "user_id"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
-    t.string "event", null: false
     t.text "detail", null: false
     t.integer "prefecture_id", null: false
-    t.integer "accept"
+    t.integer "accept_id"
     t.date "date", null: false
     t.time "start_time"
     t.time "end_time"
@@ -25,4 +34,23 @@ ActiveRecord::Schema.define(version: 2020_04_30_025620) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "profile", null: false
+    t.integer "gender", null: false
+    t.integer "age", null: false
+    t.integer "prefecture_id"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
 end
