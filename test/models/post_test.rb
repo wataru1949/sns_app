a7cefbdd3ed2f6740ed9ebd2b84a1c9b07bf8124
wataru1949 <e.phedra58@icloud.com>
2020-05-14 23:45:57 +0900

    test file

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id            :bigint           not null, primary key
+#  date          :date             not null
+#  detail        :text(65535)      not null
+#  end_time      :time
+#  start_time    :time
+#  title         :string(255)      not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  accept_id     :integer
+#  prefecture_id :integer          not null
+#
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
