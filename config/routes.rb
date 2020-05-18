70a48e02c635b5cc79a1758ec8ee7199763bc055
wataru1949 <e.phedra58@icloud.com>
2020-05-18 19:04:57 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'posts#index'
-  resources :posts
+  resources :posts do
+    resources :comments, only: :create
+  end
   resources :users, only: [:edit, :update]
 end
