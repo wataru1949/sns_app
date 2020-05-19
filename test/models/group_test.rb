7ab5a470ee33d1477af097df76e56b2851c39046
wataru1949 <e.phedra58@icloud.com>
2020-05-19 16:02:10 +0900

    test files

---
+++ test/models/group_test.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: groups
+#
+#  id            :bigint           not null, primary key
+#  content       :text(65535)      not null
+#  group_name    :string(255)      not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  prefecture_id :integer          not null
+#
+# Indexes
+#
+#  index_groups_on_group_name  (group_name) UNIQUE
+#
+require 'test_helper'
+
+class GroupTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
