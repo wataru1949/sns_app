f9cc1200629181e88cc25bf6d15d3ac8368a5eb5
wataru1949 <e.phedra58@icloud.com>
2020-05-19 16:03:36 +0900

    Create group_user.rb

---
+++ app/models/group_user.rb
@@ -0,0 +1,26 @@
+class GroupUser < ApplicationRecord
+
+  belongs_to :group
+  belongs_to :user
+end
+
+# == Schema Information
+#
+# Table name: group_users
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  group_id   :bigint
+#  user_id    :bigint
+#
+# Indexes
+#
+#  index_group_users_on_group_id  (group_id)
+#  index_group_users_on_user_id   (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (group_id => groups.id)
+#  fk_rails_...  (user_id => users.id)
+#
