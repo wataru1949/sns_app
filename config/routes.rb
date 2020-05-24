4b24d07d20b41ac904a18e047d6aaed6c32d0ab8
wataru1949 <e.phedra58@icloud.com>
2020-05-24 22:59:54 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
   resources :posts do
-    resources :comments, only: :create
+    resources :comments, only: [:create, :destroy]
   end
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:show, :edit, :update]
   resources :groups, only: [:new, :create]
 end
