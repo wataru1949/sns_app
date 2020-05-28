2818750a41d6e8df4f2e558b60ed15dc0411b504
wataru1949 <e.phedra58@icloud.com>
2020-05-28 23:00:32 +0900

    Update groups_controller.rb

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,8 +1,10 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_group, only:[:edit, :update, :show]
 
   def new
     @group = Group.new
+    @group.users << current_user
     @group.group_pictures.new
   end
 
@@ -15,8 +17,29 @@ class GroupsController < ApplicationController
     end
   end
 
+  def show
+  end
+
+
+  def edit
+  end
+
+  def update
+    if @group.save
+      #リダイレクト先変更
+      redirect_to root_path
+    else
+      render :edit
+    end
+
+  end
+
   private
   def group_params
-    params.require(:group).permit(:group_name, :content, :prefecture_id, group_pictures_attributes:[:group_image])
+    params.require(:group).permit(:group_name, :content, :prefecture_id, user_ids: [], group_pictures_attributes:[:group_image])
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 end
