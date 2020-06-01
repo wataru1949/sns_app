8818957b66ed179c434af37a6adeeafb8eb4c146
wataru1949 <e.phedra58@icloud.com>
2020-06-01 19:36:41 +0900

    add gem 'fog-aws'

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit]
-  before_action :set_post, only: [:edit, :update, :show]
+  before_action :set_post, only: [:edit, :update, :show, :destroy]
 
   def index
     @posts = Post.includes(:user).order(created_at: :desc)
@@ -36,6 +36,11 @@ class PostsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
   end
 
   private
