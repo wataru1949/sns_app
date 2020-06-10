0f5531094c192c441cedf105d24822a6afa043f4
wataru1949 <e.phedra58@icloud.com>
2020-06-10 22:48:47 +0900

    feat: グループ詳細ページでモーダル実装

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -1,8 +1,21 @@
-$(function() {
-  $('#list_show').click(function() {
-    $('#member_modal').fadeIn();
-  });
-  $('#list_close').click(function() {
-    $('#member_modal').hide();
-  });
+$(function(){
+  var openBtns = $(".member-btn");
+  function modalSwitch(){
+    $(".active").removeClass("active");
+    $(this).addClass("active");
+    const index = openBtns.index(this);
+    $(".member_modal").removeClass("show").eq(index).addClass("show");
+    $(".close-btn").click(function(){
+      $(".member_modal").removeClass("show")();
+    });
+  }
+  openBtns.click(modalSwitch);
 });
+
+
+
+
+  $(".member-btn").click(function(){
+    $(".member_modal").fadeIn();
+  });
+  
