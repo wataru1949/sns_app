572642c00f3273fabb0798efca304a6cea485340
wataru1949 <e.phedra58@icloud.com>
2020-06-17 20:06:32 +0900

    feat: before_action追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:edit]
+  before_action :authenticate_user!
+  before_action :set_user, only: [:edit, :update]
   
   def show
   end
