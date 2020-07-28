7509fece45b910d7175c39562f75b6fc3ab3703d
wataru1949 <e.phedra58@icloud.com>
2020-07-28 23:44:51 +0900

    chore: カラム名変更

--- db/migrate/20200430025620_create_posts.rb
+++ db/migrate/20200430025620_create_posts.rb
@@ -5,7 +5,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
       t.text        :detail,                       null: false
       t.integer     :prefecture_id,                null: false
       t.integer     :accept_id
-      t.date        :date,                         default: ""
+      t.date        :event_date,                   default: ""
       t.time        :start_time,                   default: ""
       t.time        :end_time,                     default: ""
       t.string      :post_image
