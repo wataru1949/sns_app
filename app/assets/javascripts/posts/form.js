d62ade9ce460008ea6c88ac6ca2743f97e778290
wataru1949 <e.phedra58@icloud.com>
2020-07-31 22:18:51 +0900

    style: クラス名変更

--- app/assets/javascripts/posts/form.js
+++ app/assets/javascripts/posts/form.js
@@ -2,8 +2,8 @@ $(function(){
   $("#post_post_image").change(function(e){
     $(".pip").remove();
     $("#post_remove").prop("checked", false);
-    if (".rm-btn") {
-      $(".rm-btn").remove();
+    if ("#post_rm") {
+      $("#post_rm").remove();
     }
     var file = e.target.files[0];
     var reader = new FileReader();
@@ -16,17 +16,17 @@ $(function(){
       var file = e.target;
       $("<span class=\"pip\">" +
         "<img class=\"post_img\" src=\"" + e.target.result + "\" title=\"" + file.name + "\"/>" + "</span>" +
-        "<br/><label class=\"rm-btn\" for=\"post_remove\">" + "<i class=\"fa fa-window-close\"></i></label>").insertAfter("#post_post_image");
-      $(".rm-btn").click(function(){
+        "<br/><label id=\"post_rm\" class=\"rm-btn\" for=\"post_remove\">" + "<i class=\"fa fa-window-close\"></i></label>").insertAfter("#post_post_image");
+      $("#post_rm").click(function(){
         $(".post_img").remove();
-        $(".rm-btn").hide()
+        $("#post_rm").hide()
         // $("#post_post_image").val("");
       });
     });
     reader.readAsDataURL(file);
   });
-  $(".rm-btn").click(function(){
+  $("#post_rm").click(function(){
     $(".post_img").remove();
-    $(".rm-btn").hide()
+    $("#post_rm").hide()
   });
 });
