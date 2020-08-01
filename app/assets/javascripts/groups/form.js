ba7b1aee8ad3be89c1f7c1cdfd15eb9760d343e0
wataru1949 <e.phedra58@icloud.com>
2020-08-01 23:53:43 +0900

    chore: グループ画像フォームのJS

--- app/assets/javascripts/groups/form.js
+++ app/assets/javascripts/groups/form.js
@@ -1,5 +1,5 @@
 $(function() {  
-  // 画像用のinputを生成する関数
+// 画像用のinputを生成する関数
   const buildFileField = (index)=> {
     const html = `<div data-index="${index}" class="js-file_group">
                     <input class="js-file" type="file"
@@ -11,44 +11,76 @@ $(function() {
   }
 
   const buildImg = (index, url)=> {
-    const html = `<img data-index="${index}" src="${url}" width="80%">`;
+    const html = `<div data-index="${index}" class="file_column">
+                    <img data-index="${index}" src="${url}" , class="group_img">
+                  </div>`;
     return html;
   }
 
   // file_fieldのnameに動的なindexをつける為の配列
   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
   // 既に使われているindexを除外
-  lastIndex = $('.js-file_group:last').data('index');
+  lastIndex = $(".js-file_group:last").data("index");
   fileIndex.splice(0, lastIndex);
-  $('.hidden-destroy').hide();
 
-  $('#image-box').on('change', '.js-file', function(e) {
-    const targetIndex = $(this).parent().data('index');
+  $(".hidden-destroy").hide();
+  $("#image-box").on("change", ".js-file", function(e) {
+    const targetIndex = $(this).parent().data("index");
     // ファイルのブラウザ上でのURLを取得する
     const file = e.target.files[0];
     const blobUrl = window.URL.createObjectURL(file);
     // 該当indexを持つimgタグがあれば取得して変数imgに入れる(画像変更の処理)
     if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
-      img.setAttribute('src', blobUrl);
+      img.setAttribute("src", blobUrl);
     } else {  // 新規画像追加の処理
-      $('#previews').append(buildImg(targetIndex, blobUrl));
+      $("#previews").append(buildImg(targetIndex, blobUrl));
+      const num = $(".js-file").length;
       // fileIndexの先頭の数字を使ってinputを作る
-      $('#image-box').append(buildFileField(fileIndex[0]));
-      fileIndex.shift();
-      // 末尾の数に1足した数を追加する
-      fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
+      if (num < 3) {
+        $("#image-box").append(buildFileField(fileIndex[0]));
+        fileIndex.shift();
+        // 末尾の数に1足した数を追加する
+        fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
+      }
+      $(".js-file_group").height(240);
+      $(".js-file_group:last").height(50);
+      lst = $("img.group_img").length;
+      // console.log(lst);
     }
   });
-
-  $('#image-box').on('click', '.js-remove', function() {
-    const targetIndex = $(this).parent().data('index')
-    // 該当indexを振られているチェックボックスを取得する
-    const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
-    // もしチェックボックスが存在すればチェックを入れる
-    if (hiddenCheck) hiddenCheck.prop('checked', true);
-    $(this).parent().remove();
-    $(`img[data-index="${targetIndex}"]`).remove();
-    // 画像入力欄が0個にならないようにしておく
-    if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
+  let lst = $("img.group_img").length;
+  // console.log(lst);
+  // :not(:last)
+  
+  $("#image-box").on("click", ".js-remove", function() {
+    if ($("img.group_img").length < 3 && $(this).parent().data("index") != $(".js-file_group:last").data("index")) {
+      const targetIndex = $(this).parent().data("index");
+      // 該当indexを振られているチェックボックスを取得する
+      const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
+      // もしチェックボックスが存在すればチェックを入れる
+      if (hiddenCheck) hiddenCheck.prop("checked", true);
+      $(this).parent().remove();
+      $(`.file_column[data-index="${targetIndex}"]`).remove();
+      $(`img[data-index="${targetIndex}"]`).remove();
+      // 画像入力欄が0個にならないようにしておく
+      if ($('.js-file').length == 0) $("#image-box").append(buildFileField(fileIndex[0]));
+    } else if ($("img.group_img").length == 3) {
+      const targetIndex = $(this).parent().data("index");
+      // 該当indexを振られているチェックボックスを取得する
+      const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
+      // もしチェックボックスが存在すればチェックを入れる
+      if (hiddenCheck) hiddenCheck.prop("checked", true);
+      $(this).parent().remove();
+      $(`.file_column[data-index="${targetIndex}"]`).remove();
+      $(`img[data-index="${targetIndex}"]`).remove();
+      $("#image-box").append(buildFileField(fileIndex[0]));
+        fileIndex.shift();
+        // 末尾の数に1足した数を追加する
+        fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
+    }
+    $(".js-file_group").height(240);
+    $(".js-file_group:last").height(50);
+    lst = $("img.group_img").length;
+    // console.log(lst);
   });
 });
