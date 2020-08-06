80e814b7cc46dc13bb64d0813008c0e264c8ecbf
wataru1949 <e.phedra58@icloud.com>
2020-08-06 21:29:31 +0900

    style: スペース削除

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -43,7 +43,7 @@ class GroupsController < ApplicationController
 
   private
   def group_params
-    params.require(:group).permit(:group_name, :content, :prefecture_id, user_ids: [], group_pictures_attributes:[:group_image,  :_destroy, :id])
+    params.require(:group).permit(:group_name, :content, :prefecture_id, user_ids: [], group_pictures_attributes:[:group_image, :_destroy, :id])
   end
 
   def set_group
