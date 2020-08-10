9e191e1001533b59736bd9e37347a81f2ef072a7
wataru1949 <e.phedra58@icloud.com>
2020-08-10 13:28:00 +0900

    chore: Addressモデルの各子モデルを作成

---
+++ app/models/post_address.rb
@@ -0,0 +1,21 @@
+class PostAddress < Address
+  validates :prefecture_id, presence: { message: "を入力してください" }
+end
+
+# == Schema Information
+#
+# Table name: addresses
+#
+#  id               :bigint           not null, primary key
+#  addressable_type :string(255)
+#  city             :string(255)
+#  place            :string(255)
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  addressable_id   :bigint
+#  prefecture_id    :integer
+#
+# Indexes
+#
+#  index_addresses_on_addressable_type_and_addressable_id  (addressable_type,addressable_id)
+#
