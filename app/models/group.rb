c3fa959d71dad97357ef031f14aba107914384ef
wataru1949 <e.phedra58@icloud.com>
2020-08-14 18:24:17 +0900

    chore: postとgroupモデルにアソシエーション設定

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,13 +1,14 @@
 class Group < ApplicationRecord
 
+  belongs_to :category
   has_many :group_users, dependent: :destroy
   has_many :users, through: :group_users
   has_many :group_pictures, dependent: :destroy
   has_many :chats, dependent: :destroy
   has_one :group_address, as: :addressable, dependent: :destroy
-  # belongs_to :category
   accepts_nested_attributes_for :group_pictures, reject_if: proc { |attributes| attributes["group_image"].blank? },allow_destroy: true
   accepts_nested_attributes_for :group_address
+  accepts_nested_attributes_for :category
 
   validates :content, :group_name, presence: true
   validates :group_name, uniqueness: true, length: { maximum: 20 }
@@ -23,13 +24,19 @@ end
 #
 # Table name: groups
 #
-#  id         :bigint           not null, primary key
-#  content    :text(65535)      not null
-#  group_name :string(255)      not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id          :bigint           not null, primary key
+#  content     :text(65535)      not null
+#  group_name  :string(255)      not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  category_id :bigint
 #
 # Indexes
 #
-#  index_groups_on_group_name  (group_name) UNIQUE
+#  index_groups_on_category_id  (category_id)
+#  index_groups_on_group_name   (group_name) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (category_id => categories.id)
 #
