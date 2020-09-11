a6d1ea9dce970eac1ca9698e2dc66e426acdd7ff
wataru1949 <e.phedra58@icloud.com>
2020-09-11 11:29:16 +0900

    feat: 投稿とグループ検索フォームで使うフォームオブジェクトを定義

--- app/forms/post_search_form.rb
+++ app/forms/post_search_form.rb
@@ -1,14 +1,17 @@
 class PostSearchForm
   include ActiveModel::Model
+  include StringNormalizer
 
   attr_accessor :category, :event_date, :prefecture_id, :city
 
   def search
+    normalize_values
+
     rel = Post
 
     if category.present?
       rel = rel.joins(:category)
-      rel = rel.where("categories.value" => category)
+      rel = rel.where("categories.value like ?", "%#{category}%")
     end
 
     if event_date.present?
@@ -21,11 +24,16 @@ class PostSearchForm
         rel = rel.where("addresses.prefecture_id" => prefecture_id)
       end
 
-      rel = rel.where("addresses.city" => city) if city.present?
+      rel = rel.where("addresses.city like ?", "%#{city}%") if city.present?
     end
 
     rel = rel.distinct
     
     rel.post_listing
+  end
+
+  private def normalize_values
+    self.category = normalize_as_generic(category)
+    self.city = normalize_as_generic(city)
   end
 end
