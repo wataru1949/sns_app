abe71bfe684ceb1d2600a227448b839897af7c05
wataru1949 <e.phedra58@icloud.com>
2020-10-03 22:15:39 +0900

    feat: GroupMemberモデル定義

---
+++ app/models/group_member.rb
@@ -0,0 +1,25 @@
+class GroupMember < GroupUser
+end
+
+# == Schema Information
+#
+# Table name: group_users
+#
+#  id         :bigint           not null, primary key
+#  approved   :boolean          default(FALSE), not null
+#  type       :string(255)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  group_id   :bigint
+#  user_id    :bigint
+#
+# Indexes
+#
+#  index_group_users_on_group_id  (group_id)
+#  index_group_users_on_user_id   (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (group_id => groups.id)
+#  fk_rails_...  (user_id => users.id)
+#
