6648f2c2316f7909f5ef1e5699093c23beedd2e2
wataru1949 <e.phedra58@icloud.com>
2020-10-06 17:59:12 +0900

    feat: group_membersコントローラーにcreateアクション追加

--- app/controllers/group_members_controller.rb
+++ app/controllers/group_members_controller.rb
@@ -5,4 +5,10 @@ class GroupMembersController < ApplicationController
     @group = Group.find(params[:group_id])
   end
 
+  def create
+    @group = Group.find(params[:group_id])
+    @group.group_members.create(user: current_user)
+    flash.notice = "参加申請しました。"
+    redirect_to group_path(@group)
+  end
 end
