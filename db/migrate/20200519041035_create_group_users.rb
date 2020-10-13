0a59a75e35c4a07662887383c6a3ab957fb568dd
wataru1949 <e.phedra58@icloud.com>
2020-10-13 18:03:33 +0900

    feat: 中間テーブルにカラムを追加
    状態を表すカラムと拒否フラグのカラム

--- db/migrate/20200519041035_create_group_users.rb
+++ db/migrate/20200519041035_create_group_users.rb
@@ -1,13 +1,14 @@
 class CreateGroupUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :group_users do |t|
-      t.references :group,  foreign_key: true
-      t.references :user,   foreign_key: true
-      t.boolean :approved,  null: false, default: false
-      t.string :type,       null: false
-
+      t.references :group,     foreign_key: true
+      t.references :user,      foreign_key: true
+      t.string :type,          null: false
+      t.string :status,        null: false
+      t.boolean :rejected      null: false, default: false
       t.timestamps
     end
     add_index :group_users, [ :group_id, :user_id ], unique: true
+    add_index :group_usres, [ :type ]
   end
 end
