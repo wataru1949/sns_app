66140caa43bf2786e83167b9e6706d3e568a1003
wataru1949 <e.phedra58@icloud.com>
2020-10-16 16:10:08 +0900

    feat: group_membersテーブルのカラム変更
    状態管理と拒否フラグのカラム

--- db/migrate/20200519041035_create_group_users.rb
+++ db/migrate/20200519041035_create_group_users.rb
@@ -5,10 +5,10 @@ class CreateGroupUsers < ActiveRecord::Migration[5.2]
       t.references :user,      foreign_key: true
       t.string :type,          null: false
       t.string :status,        null: false
-      t.boolean :rejected      null: false, default: false
+      t.boolean :rejected,     null: false, default: false
       t.timestamps
     end
     add_index :group_users, [ :group_id, :user_id ], unique: true
-    add_index :group_usres, [ :type ]
+    add_index :group_users, [ :status ]
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -65,11 +65,14 @@ ActiveRecord::Schema.define(version: 2020_08_13_061508) do
   create_table "group_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "group_id"
     t.bigint "user_id"
-    t.boolean "approved", default: false, null: false
     t.string "type", null: false
+    t.string "status", null: false
+    t.boolean "rejected", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["group_id", "user_id"], name: "index_group_users_on_group_id_and_user_id", unique: true
     t.index ["group_id"], name: "index_group_users_on_group_id"
+    t.index ["status"], name: "index_group_users_on_status"
     t.index ["user_id"], name: "index_group_users_on_user_id"
   end
 
