c7b9f43699282b51418b29e9a7c696055b5c3fc5
wataru1949 <e.phedra58@icloud.com>
2020-10-19 21:58:33 +0900

    chore: 申請処理のアクションで条件追加

--- app/controllers/group_members_controller.rb
+++ app/controllers/group_members_controller.rb
@@ -3,11 +3,13 @@ class GroupMembersController < ApplicationController
 
   def index
     @group = Group.find(params[:group_id])
+    # @applying_member = @group.group_members.where(status: "applying", rejected: false).includes(:user)
+    
   end
 
   def create
     @group = Group.find(params[:group_id])
-    @group.group_members.create(user: current_user)
+    @group.group_members.create(user: current_user, status: "applying")
     flash.notice = "参加申請しました。"
     redirect_to group_path(@group)
   end
@@ -15,8 +17,13 @@ class GroupMembersController < ApplicationController
   def destroy
     @group = Group.find(params[:group_id])
     members = @group.group_members.find_by(user_id: current_user.id)
-    members.destroy
-    flash.notice = "申請を取り消しました。"
+    if members.status == "participated"
+      members.destroy
+      flash.notice = "グループから退会しました"
+    else
+      members.destroy
+      flash.notice = "申請を取り消しました。"
+    end
     redirect_to group_path(@group)
   end
 end
