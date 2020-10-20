20d085178bc8514a9b993e4a6f06411251cb05a7
wataru1949 <e.phedra58@icloud.com>
2020-10-20 23:54:14 +0900

    style: JSファイルの不要部分を削除

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -20,7 +20,6 @@ $(function(){
       $(".rm-btn").click(function(){
         $(".user_img_d").remove();
         $(".rm-btn").hide()
-        // $("#post_post_image").val("");
       });
     });
     reader.readAsDataURL(file);
@@ -30,23 +29,3 @@ $(function(){
     $(".rm-btn").hide()
   });
 });
-
-
-// $(function(){
-//   $('#user_user_image').change(function(e){
-//     var file = e.target.files[0];
-//     var reader = new FileReader();
-//     if(file.type.indexOf("image") < 0){
-//       alert("画像ファイルを指定してください。");
-//       $(this.form).find(":file").val("");
-//       return false;
-//     }
-//     reader.onload = (function(file){
-//       return function(e){
-//         $(".user_image").attr("src", e.target.result);
-//         $(".user_image").attr("title", file.name);
-//       };
-//     })(file);
-//     reader.readAsDataURL(file);
-//   });
-// });
