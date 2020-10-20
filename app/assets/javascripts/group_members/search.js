ecb328ef4c65e7aad83710c6722938a298bda3f2
wataru1949 <e.phedra58@icloud.com>
2020-10-20 23:55:55 +0900

    feat: ユーザー検索と招待までのJSの動きを修正

--- app/assets/javascripts/group_members/search.js
+++ app/assets/javascripts/group_members/search.js
@@ -1,13 +1,38 @@
 $(function() {
-  function addUser(user) {
-    let html = `
-      <div class="searched-user">
-        <p class="searched-user_name">${user.nickname}</p>
-        <div class="user-add_btn" data-user-id="${user.id}" data-user-nickname="${user.nickname}">追加</div>
-      </div>
-    `;
-    $("#user-search-result").append(html);
-  }
+  function addUser(user, groupId) {
+    if (user.image) {
+      let html = `
+        <div class="list_area_sell">
+          <div class="list_area_sell_left">
+            <img alt="ユーザー画像" class="user_img" src="${user.image}">
+          </div>
+          <div class="list_area_sell_right">
+            <div class="list_area_sell_right_name">${user.nickname}</div>
+            <div class="list_area_sell_right_btn">
+              <a class="link-btn btn"  data-user_id ${user.id} data-confirm="${user.nickname}さんを招待しますか？" rel="nofollow" data-method="post" href="/groups/${groupId}/members/${user.id}">招待</a>
+            </div>
+          </div>
+        </div>
+      `;
+      $("#user-search-result").append(html);
+    } else {
+      let html = `
+        <div class="list_area_sell">
+          <div class="list_area_sell_left">
+            <img alt="ユーザー画像" class="user_img" src="/assets/picture/user-2517433_640.png">
+          </div>
+          <div class="list_area_sell_right">
+            <div class="list_area_sell_right_name">${user.nickname}</div>
+            <div class="list_area_sell_right_btn">
+              <a class="link-btn btn" data-confirm="${user.nickname}さんを招待しますか？" rel="nofollow" data-method="post" href="/groups/${groupId}/members">招待</a>
+            </div>
+          </div>
+        </div>
+      `;
+      $("#user-search-result").append(html);
+    };
+  };
+
   function addNoUser() {
     let html = `
       <div class="searched-user">
@@ -17,21 +42,24 @@ $(function() {
     $("#user-search-result").append(html);
   }
 
-  function addDeleteUser(name, id) {
-    let html = `
-    <div class="searched-user added_user" id="${id}">
-      <p class="searched-user_name">${name}</p>
-      <div class="user-remove_btn" data-user-id="${id}" data-user-name="${name}">削除</div>
-    </div>`;
-    $("#added-user_container").append(html);
-  }
-  function addMember(userId) {
-    let html = `<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" />`;
-    $(`#${userId}`).append(html);
-  }
 
-  $(".user_search_form-input").on("keyup", function() {
+
+  // function addInvitedUser(name, id) {
+  //   let html = `
+  //   <div class="searched-user added_user" id="${id}">
+  //     <p class="searched-user_name">${name}</p>
+  //     <div class="user-remove_btn" data-user-id="${id}" data-user-name="${name}">削除</div>
+  //   </div>`;
+  //   $("#invited_area").append(html);
+  // }
+  // function addMember(userId) {
+  //   let html = `<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" />`;
+  //   $(`#${userId}`).append(html);
+  // }
+
+  $(".user_search_form-btn").on("click", function() {
     let input = $(".user_search_form-input").val();
+    let groupId = $(this).attr("data-group-id");
     $.ajax({
       type: 'GET',
       url: '/users/search',
@@ -42,7 +70,7 @@ $(function() {
       $("#user-search-result").empty();
       if (users.length !== 0) {
         users.forEach(function(user) {
-          addUser(user);
+          addUser(user, groupId);
         });
       } else if (input.length == 0) {
         return false;
@@ -55,18 +83,18 @@ $(function() {
     });
   });
 
-  $(document).on("click", ".user-add_btn", function() {
-    const userName = $(this).attr("data-user-nickname");
-    const userId = $(this).attr("data-user-id");
-    $(this)
-      .parent()
-      .remove();
-    addDeleteUser(userName, userId);
-    addMember(userId);
-  });
-  $(document).on("click", ".user-remove_btn", function() {
-    $(this)
-      .parent()
-      .remove();
-  });
+  // $(document).on("click", ".user-add_btn", function() {
+  //   const userName = $(this).attr("data-user-nickname");
+  //   const userId = $(this).attr("data-user-id");
+  //   $(this)
+  //     .parent()
+  //     .remove();
+  //   addInvitedUser(userName, userId);
+  //   addMember(userId);
+  // });
+  // $(document).on("click", ".user-remove_btn", function() {
+  //   $(this)
+  //     .parent()
+  //     .remove();
+  // });
 });
