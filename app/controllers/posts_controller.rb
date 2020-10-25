35ac577b8e721c116c1cdfa6125ce3f754ae378c
wataru1949 <e.phedra58@icloud.com>
2020-10-25 10:13:39 +0900

    chore: onlyをexceptに変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_post, only: [:edit, :update, :show, :destroy]
 
   def index
