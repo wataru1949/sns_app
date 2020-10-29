abe25cec8e205bceaf2769fd5ba101f1dd5f56de
wataru1949 <e.phedra58@icloud.com>
2020-10-29 22:13:04 +0900

    style: コメント削除

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,27 +1,4 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id                     :bigint           not null, primary key
-#  age                    :integer          default("age_private")
-#  email                  :string(255)      default(""), not null
-#  encrypted_password     :string(255)      default(""), not null
-#  gender                 :integer          default("gender_private")
-#  nickname               :string(255)      not null
-#  profile                :string(255)
-#  remember_created_at    :datetime
-#  reset_password_sent_at :datetime
-#  reset_password_token   :string(255)
-#  user_image             :string(255)
-#  created_at             :datetime         not null
-#  updated_at             :datetime         not null
-#
-# Indexes
-#
-#  index_users_on_email                 (email) UNIQUE
-#  index_users_on_nickname              (nickname) UNIQUE
-#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
-#
+
 require "rails_helper"
 
 describe User do
