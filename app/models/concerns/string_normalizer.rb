8f51b3bce0b1c0032614753ef409a0c332c47e6f
wataru1949 <e.phedra58@icloud.com>
2020-11-01 20:05:03 +0900

    feat: email正規化のメソッドを定義

--- app/models/concerns/string_normalizer.rb
+++ app/models/concerns/string_normalizer.rb
@@ -6,4 +6,8 @@ module  StringNormalizer
   def normalize_as_generic(text)
     NKF.nkf("-W -w -Z1 -X", text).strip if text
   end
+
+  def normalize_as_email(text)
+    NKF.nkf("-W -w -Z1", text).strip if text
+  end
 end
