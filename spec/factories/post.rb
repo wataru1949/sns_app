7d19dcf1fafe09008f9f15ffc45d8ade75031c2b
wataru1949 <e.phedra58@icloud.com>
2020-11-02 20:01:17 +0900

    fix: 投稿のファクトリー修正

--- spec/factories/post.rb
+++ spec/factories/post.rb
@@ -3,13 +3,13 @@ FactoryBot.define do
   factory :post do
     title          {Faker::Lorem.sentence}
     detail         {Faker::Lorem.sentence}
-    prefecture_id  {Faker::Number.within(range: 1..48)}
     accept_id      {Faker::Number.within(range: 1..4)}
-    event_date           {Faker::Date.in_date_period}
+    event_date     {Date.today}
     start_time     {"21:00:00"}
     end_time       {"21:00:00"}
     post_image     {File.open("#{Rails.root}/public/images/test_image.jpg")}
     created_at     { Faker::Time.between(from: DateTime.now - 2, to: DateTime.now) }
+    category
     user
   end
 
