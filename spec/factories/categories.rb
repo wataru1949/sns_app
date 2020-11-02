579c2e28202da2d94c5637a71f61892f24529779
wataru1949 <e.phedra58@icloud.com>
2020-11-02 20:00:10 +0900

    style: コメント削除

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,18 +1,6 @@
-# == Schema Information
-#
-# Table name: categories
-#
-#  id         :bigint           not null, primary key
-#  value      :string(255)      not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
-# Indexes
-#
-#  index_categories_on_value  (value) UNIQUE
-#
 FactoryBot.define do
+  
   factory :category do
-    
+    value {"野球"}
   end
 end
