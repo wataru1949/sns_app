b6eee2c6505a301419ae552a66f0ecfac34eb75e
wataru1949 <e.phedra58@icloud.com>
2020-11-02 20:01:02 +0900

    fix: グループのファクトリー修正

--- spec/factories/group.rb
+++ spec/factories/group.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
   factory :group do
     group_name     {Faker::Team.name}
     content        {Faker::Lorem.sentence}
-    prefecture_id  {Faker::Number.within(range: 1..48)}
+    category_id    {1}
+    association :category
   end
-
 end
