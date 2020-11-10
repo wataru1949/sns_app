b94cbfa096895600d9a3654ce64a94309351a070
wataru1949 <e.phedra58@icloud.com>
2020-11-10 16:30:50 +0900

    chore: ファクトリーのアソシエーション定義

--- spec/factories/group.rb
+++ spec/factories/group.rb
@@ -3,7 +3,8 @@ FactoryBot.define do
   factory :group do
     group_name     {Faker::Team.name}
     content        {Faker::Lorem.sentence}
-    category_id    {1}
     association :category
+    association :group_address, strategy: :build
+    association :group_admin, strategy: :build
   end
 end
