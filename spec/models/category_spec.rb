e0605435bd170e89c180d340430e8815c6eff1f8
wataru1949 <e.phedra58@icloud.com>
2020-11-12 22:37:38 +0900

    test: categoryモデルの単体テスト

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,27 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Category do
+  describe "値の正規化" do
+    example "value前後の空白を除去" do
+      category = create(:category, value: " 野球 ")
+      expect(category.value).to eq("野球")  
+    end
+
+    example "valueに含まれる全角英数字記号を半角に変換" do
+      category = create(:category, value: "ｂａｓｅｂａｌｌ")
+      expect(category.value).to eq("baseball")
+    end
+
+    example "value前後の全角スペースを除去" do
+      category = create(:category, value: "\u{3000}サッカー\u{3000}")
+      expect(category.value).to eq("サッカー")  
+    end
+  end
+
+  describe "バリデーション" do
+    example "記号を含むvalueは無効" do
+      category = build(:category, value: "試験★")
+      expect(category).not_to be_valid  
+    end
+  end
 end
